1cb39f7712a34fa354e808d4924c5df5ce8732be
sagar <sagarbhadrawle@gmail.com>
2025-07-16 22:53:53 +0530

    http:

---
+++ MyFirstProject/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+	
+	@GetMapping(value="/hello")
+	public String getMessage()
+	{
+		return "Hello_ Masai";
+	}
+	
+	
+}
